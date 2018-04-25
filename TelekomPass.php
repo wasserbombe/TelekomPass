3b7f32d09365bdbfa16bc11387aa32eed361177f
Sebastian Fuhrmann <info@sebastianfuhrmann.de>
2018-04-25 23:16:22 +0200

    added getStatus()

--- TelekomPass.php
+++ TelekomPass.php
@@ -37,6 +37,89 @@
 				return $doc->textContent; 
 			}
 
+			/**
+			 * Get current pass status from home page
+			 */
+			public function getStatus(){
+				$res = array(
+					"activePass" => null,
+					"billingPeriod" => null,
+					"remainingTime_text" => null,
+					"remainingTime" => null,
+					"tariffZone" => null,
+					"data" => array(
+						"currentDataUsage" => null,
+						"currentDataPercentage" => null,
+						"dataVolume" => null,
+					)
+				);
+				$doc = $this->getHTML($this->endpoint . 'home');
+
+				foreach ($doc->getElementsByTagName("h2") as $h2){
+					if ($h2->getAttribute("id") == "pageTitle"){
+						$res["activePass"] = $h2->textContent;
+					}
+				}
+
+				// parse info lines
+				$trs = $doc->getElementsByTagName("tr");
+				foreach ($trs as $tr){
+					if (!$this->hasClass($tr, "infoLine")) continue;
+
+					foreach ($tr->getElementsByTagName("td") as $td){
+						if ($this->hasClass($td, "infoValue")){
+							if ($this->hasClass($td, "billingPeriod")){
+								$res["billingPeriod"] = $td->textContent;
+							} elseif ($this->hasClass($td, "remainingTime")){
+								$res["remainingTime_text"] = $td->textContent;
+
+								$res["remainingTime"] = 0; 
+
+								// parse days
+								if (preg_match("~([0-9]+) Tage?~", $res["remainingTime_text"], $matches)){
+									$res["remainingTime"] += (int)$matches[1]*60*60*24;
+								}
+
+								// parse hours
+								if (preg_match("~([0-9]+) Std~", $res["remainingTime_text"], $matches)){
+									$res["remainingTime"] += (int)$matches[1]*60*60;
+								}
+
+								// parse minutes
+								if (preg_match("~([0-9]+) Min\.~", $res["remainingTime_text"], $matches)){
+									$res["remainingTime"] += (int)$matches[1]*60;
+								}
+
+								// TODO: Are there more parts?! 
+
+							} elseif ($this->hasClass($td, "tariffZone")){
+								$res["tariffZone"] = $td->textContent;
+							} else {
+								// unknown info
+							}
+						} 
+					}
+				}
+
+				// parse data volume
+				foreach ($doc->getElementsByTagName("div") as $div){
+					if (!$this->hasClass($div, "barTextBelow")) continue; 
+
+					if (preg_match("~([0-9]+,?[0-9]*).{2}(KB|GB) von ([0-9]+,?[0-9]*).{2}(KB|GB)~i", $div->textContent, $matches)){
+						$res["data"]["currentDataUsage"] = (double)str_replace(",",".", $matches[1]);
+						
+						$res["data"]["dataVolume"] = (double)str_replace(",",".", $matches[3]);
+						$res["data"]["currentDataPercentage"] = $res["data"]["currentDataUsage"]/$res["data"]["dataVolume"];
+
+						break;
+					}
+				}
+
+				return $res;
+
+			}
+
+
 			//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 			// HELPER FUNCTIONS //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 			//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
